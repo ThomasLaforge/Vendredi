27c7825a0ffba9ad4658d85ad65b37e52f44ad62
ThomasLaforge <thomas.laforge.38@gmail.com>
2016-01-25 23:15:19 +0100

    First commit

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+// Jquery et bootstrap
+window.jQuery = window.$ = require('jquery');
+var bootstrap = require('bootstrap/dist/js/bootstrap');
+
+var example = require("./example");
+example.welcome();
+
