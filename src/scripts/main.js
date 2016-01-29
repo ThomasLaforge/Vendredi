0e036f15397eecc4a1f489d7341182ded01af1c2
ThomasLaforge <thomas.laforge.38@gmail.com>
2016-01-29 17:01:09 +0100

    regles et tests d'heritage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,3 +12,27 @@ import {Deck} from './Deck';
 var myDeck = new Deck();
 myDeck.shuffleDeck();
 console.log(myDeck.pickCards(1));
+
+class Carte {
+	constructor(){
+		console.log('Création d\'une nouvelle carte');
+	}
+}
+
+class CarteDanger extends Carte {
+	constructor(lvl1,lvl2,lvl3){
+		this._lvl_1 = lvl1;
+		this._lvl_2 = lvl2;
+		this._lvl_3 = lvl3;
+	}
+}
+
+class CarteCombat extends Carte {
+	constructor(lvl1,lvl2,lvl3){
+		
+	}
+}
+
+class DeckCarte {
+
+}
