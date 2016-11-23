d0bd4d75ccf05a44f8bea8ab5bb634692c641aa1
Thomas Laforge <thomas.laforge.38@gmail.com>
2016-11-23 09:15:37 +0100

    adding fight pirate power in decount

--- src/scripts/PirateFight.js
+++ src/scripts/PirateFight.js
@@ -6,7 +6,26 @@ class PirateFight extends Fight {
     }
 
     getPlayerForce(){
-        let res = super();
+        let playerForce = 0;
+        
+        if( this.cardToFight.id == 6 ){
+
+        }
+        else if( this.cardToFight.id == 9 ){
+            
+        }
+        else {
+            playerForce = super();
+        }
+
+        return playerForce;
+    }
+
+    getStrengthCardToFight(){
+        let nbAgingCardUsed = 0;
+
+
+        let res = 0;
         if(this.cardToFight.strength != null){
             res = this.cardToFight.strength;
         }
@@ -14,8 +33,13 @@ class PirateFight extends Fight {
         if(this.cardToFight.mission != null){
             switch (this.cardToFight.id) {
                 case 0:
-                case 1:
+                    res = -1;
                     // Fight all cards => 0. No instructions cause res already equal to 0
+                case 1:
+                    res = 2 * nbAgingCardUsed;
+                    break;
+                case 4:
+
                     break;
                 default:
                     break;
@@ -23,6 +47,7 @@ class PirateFight extends Fight {
         }
         return res;
     }
+
 }
 
 export { PirateFight }
