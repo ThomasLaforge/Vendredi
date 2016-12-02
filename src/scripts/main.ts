cfb822b87fd04f48cd4d1885bc0ca04742fafe54
Thomas Laforge <thomas.laforge.38@gmail.com>
2016-12-02 19:37:54 +0100

    add template and completing zone info

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -31,22 +31,69 @@ watch(game, "_fight", () => {
 });
 
 // Interface
-
 // $('a[data-toggle="popover"]').popover({
 //     animated: 'fade',
 //     placement: 'bottom',
 //     html: true
 // });
 
+Vue.component('gameInfo', {
+    props : ['game'],
+    template : `
+    <div class="game-info">
+        <div class="info-main ">
+            <div class='info-main-pv info-main-elt' id="pv ">
+                <div class='info-main-value'>{{ PV }}</div>
+                <!--<button @click="loseOnePV ">LosePV</button>-->
+            </div>
+            <div class='info-main-fight-cards info-main-elt'>
+                <div id="nbFightCards " class='info-main-value'>{{ nbFightCardsInDeck }}</div>
+                <div class='info-main-subject'>Fight cards</div>
+            </div>
+            <div class='info-main-danger-cards info-main-elt'>
+                <div id="nbDangerCards " class='info-main-value'>{{ nbDangerCardsInDeck }}</div>
+                <div class='info-main-subject'>Danger cards</div>
+            </div>
+            <div class='info-main-aging-cards info-main-elt'>
+                <div id="nbAgingCards " class='info-main-value'>{{ nbAgingCardsInDeck }}</div>
+                <div class='info-main-subject'>Aging cards</div>
+            </div>
+            <div class='info-main-level info-main-elt'>
+                <div id="level " class='info-main-value'>{{ level }}</div>
+                <div class='info-main-subject'>Level</div>
+            </div>
+        </div>
+    </div>
+    `,
+    methods : {
+        loseOnePV : function() {
+            this.game.player.losePV(1)
+        }
+    },
+    computed: {
+        nbFightCardsInDeck: () => {
+            return game.fightDeck.length()
+        },
+        nbDangerCardsInDeck: () => {
+            return game.dangerDeck.length()
+        },
+        nbAgingCardsInDeck: () => {
+            return game.agingDeck.length()
+        },
+        level: () => {
+            return game.level
+        },
+        PV : () => {
+            return game.player.PV
+        }
+    }
+});
+
 var app = new Vue({
-    el: '#pv',
+    el: '#app',
     data: {
         game : game
     },
-
     methods: {
-    losePV: function (event) {
-      game.loseOnePV();
     }
-  }
 })
