fc3656195f74c04de97a2097a57c521e52002092
ThomasLaforge <thomas.laforge.38@gmail.com>
2016-12-11 16:46:47 +0100

    upgrade gameover informations

--- src/scripts/Vue/gameOver.ts
+++ src/scripts/Vue/gameOver.ts
@@ -1,11 +1,18 @@
 let template = `
 <div>
-    GAME OVER !
+    <img class="game-over-img" src="../../images/game_over.png"></img>
+    <div class="game-over-sum-up">
+        <h3>Dommage {{ playerName }}, voici le résumé de votre partie :</h2>
+        <h4>Votre deck :</h4>
+        <h4>Phase combat :</h4>
+        <h4>Phase pirate :</h4>
+    </div>
 </div>
 `
 
 const gameOver = {
     template : template,
+    props : ['playerName']
 }
 
 export { gameOver }
